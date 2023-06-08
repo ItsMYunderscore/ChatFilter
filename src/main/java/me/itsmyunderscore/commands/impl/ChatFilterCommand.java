eea8bddf143d1b09fe5b6d904a6dac6827e1ab22
ItsMY_ <itsmyunderscore.sg@gmail.com>
2023-06-08 22:26:33 +0200

    changelist updated

--- src/main/java/me/itsmyunderscore/commands/impl/ChatFilterCommand.java
+++ src/main/java/me/itsmyunderscore/commands/impl/ChatFilterCommand.java
@@ -22,7 +22,7 @@ public class ChatFilterCommand extends AbstractCommand {
         addSubcommand("changelog", "Shows changelog", "/ChatFilter changelog", (player, args) -> {
             String[] changes = {"1.0.0 - Initial commit", "1.0.1 - /cfdebug - added, /filter - development began", "1.0.2 - /filter - Word manager feature added",
                     "1.0.3 - /filter - Settings feature added", "1.0.4 - /chatfilter - development began, /filter - bug fixes", "1.0.5 - /filter - reload feature added",
-                    "1.0.6 - Lang - added", "1.0.7 - URL & IP filtering added", "1.0.8 - /chatfilter - features added", "1.0.9 - Bug fixes & optimization", "1.1.0- /cflang - feature added"};
+                    "1.0.6 - Lang - added", "1.0.7 - URL & IP filtering added", "1.0.8 - /chatfilter - features added", "1.0.9 - Bug fixes & optimization", "1.1.0 - /cflang - feature added", "1.2.0 - refactoring @ implementation of bruteforceing hidden characters representing letters in messages & added db support"};
 
             player.sendMessage(StringUtil.color("&8&m---------------------Changelog---------------------"));
             for (String change : changes) {
